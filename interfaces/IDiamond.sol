0c536c54ba8e6ff881d3c22fb64223af3acd76c2
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-03-11 07:13:33 +0900

    Update - Solidity Version

--- interfaces/IDiamond.sol
+++ interfaces/IDiamond.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: CC0-1.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
 /******************************************************************************\
-* Author: Nick Mudge <nick@perfectabstractions.com>, Twitter/Github: @mudgen
-* Modifier : Coinmeca Team <dev@coinmeca.net>
+* Author: Coinmeca Team <dev@coinmeca.net>
 * Lightweight version of EIP-2535 Diamonds
 \******************************************************************************/
 
