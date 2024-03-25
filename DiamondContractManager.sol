a531db2e6bded131543a8bd04b6d75ec37e432fa
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-03-25 22:53:00 +0900

    Update - Security Access

--- DiamondContractManager.sol
+++ DiamondContractManager.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.24;
 * Lightweight version of EIP-2535 Diamonds
 \******************************************************************************/
 
-import {IDiamond} from "./interfaces/IDiamond.sol";
+import { IDiamond } from "./interfaces/IDiamond.sol";
 
 library DiamondContractManager {
     using DiamondContractManager for bytes32;
@@ -43,24 +43,24 @@ library DiamondContractManager {
 
     /* Ownable */
 
-    function setOwner(bytes32 _key, address _owner) internal {
-        enforceIsContractOwner(_key);
-        Data storage $ = diamond(_key);
+    function initialOwner(address _owner) internal {
+        diamond(base).owner = _owner;
+    }
+
+    function setOwner(address _owner) internal {
+        Data storage $ = diamond(base);
+        enforceIsContractOwner();
         $.owner = _owner;
-        $.access[_owner] = true;
         emit OwnershipTransferred($.owner, _owner);
     }
 
-    function owner(bytes32 _key) internal view returns (address _owner) {
-        _owner = diamond(_key).owner;
+    function owner() internal view returns (address _owner) {
+        _owner = diamond(base).owner;
     }
 
-    function enforceIsContractOwner(bytes32 _key) internal view {
-        Data storage $ = diamond(_key);
-        if ($.owner != address(0))
-            if (msg.sender != $.owner) {
-                revert IDiamond.NotContractOwner(msg.sender, $.owner);
-            }
+    function enforceIsContractOwner() internal view {
+        Data storage $ = diamond(base);
+        if ($.owner != msg.sender) revert IDiamond.NotContractOwner(msg.sender, $.owner);
     }
 
     /* Access */
@@ -70,39 +70,26 @@ library DiamondContractManager {
         diamond(_key).access[_owner] = _access;
     }
 
-    function checkAccess(
-        bytes32 _key,
-        address _owner
-    ) internal view returns (bool check) {
+    function checkAccess(bytes32 _key, address _owner) internal view returns (bool check) {
         Data storage $ = diamond(_key);
-        check = $.access[_owner];
+        if (diamond(base).owner == _owner || $.access[_owner]) check = true;
         if (!check) revert IDiamond.AccessDenied(_owner);
         return check;
     }
 
     /* Loupe */
 
-    function functs(
-        bytes32 _key,
-        address _facet
-    ) internal view returns (bytes4[] memory _functs) {
+    function functs(bytes32 _key, address _facet) internal view returns (bytes4[] memory _functs) {
         _functs = diamond(_key).facet[_facet].functs;
-        return
-            _functs.length == 0 ? diamond(base).facet[_facet].functs : _functs;
+        return _functs.length == 0 ? diamond(base).facet[_facet].functs : _functs;
     }
 
-    function facet(
-        bytes32 _key,
-        bytes4 _funct
-    ) internal view returns (address _facet) {
+    function facet(bytes32 _key, bytes4 _funct) internal view returns (address _facet) {
         _facet = diamond(_key).funct[_funct].facet;
-        return
-            _facet == address(0) ? diamond(base).funct[_funct].facet : _facet;
+        return _facet == address(0) ? diamond(base).funct[_funct].facet : _facet;
     }
 
-    function facets(
-        bytes32 _key
-    ) internal view returns (address[] memory _facets) {
+    function facets(bytes32 _key) internal view returns (address[] memory _facets) {
         address[] memory f = diamond(_key).facets;
         uint l = f.length;
         if (f.length > 0)
@@ -116,9 +103,7 @@ library DiamondContractManager {
             }
     }
 
-    function getFacets(
-        bytes32 _key
-    ) internal view returns (Facet[] memory facets_) {
+    function getFacets(bytes32 _key) internal view returns (Facet[] memory facets_) {
         Data storage $ = diamond(_key);
         uint length = $.facets.length;
         facets_ = new Facet[](length + 1);
@@ -126,66 +111,38 @@ library DiamondContractManager {
             address facet_ = $.facets[i];
             facets_[i] = Facet(facet_, $.facet[facet_].functs);
         }
-        facets_[length] = Facet(
-            address(this),
-            diamond(base).facet[address(this)].functs
-        );
+        facets_[length] = Facet(address(this), diamond(base).facet[address(this)].functs);
     }
 
-    function setInterface(
-        bytes32 _key,
-        bytes32 _service,
-        bytes4 _interface,
-        bool _state
-    ) internal {
-        _key.checkAccess(msg.sender);
-        diamond(_service).interfaces[_interface] = _state;
+    function setInterface(bytes32 _key, bytes4 _interface, bool _state) internal {
+        checkAccess(_key, msg.sender);
+        diamond(_key).interfaces[_interface] = _state;
     }
 
-    function checkInterface(
-        bytes32 _service,
-        bytes4 _interface
-    ) internal view returns (bool) {
-        return diamond(_service).interfaces[_interface];
+    function checkInterface(bytes32 _key, bytes4 _interface) internal view returns (bool) {
+        return diamond(_key).interfaces[_interface];
     }
 
     /* DiamondCut */
 
-    event DiamondCut(
-        IDiamond.Data[] _diamondCut,
-        address _init,
-        bytes _calldata
-    );
+    event DiamondCut(IDiamond.Data[] _diamondCut, address _init, bytes _calldata);
 
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     // Internal function version of diamondCut
-    function diamondCut(
-        IDiamond.Cut[] memory _diamondCut,
-        address _init,
-        bytes memory _calldata
-    ) internal {
+    function diamondCut(IDiamond.Cut[] memory _diamondCut, address _init, bytes memory _calldata) internal {
         for (uint i; i < _diamondCut.length; ++i) {
+            bytes32 key = keccak256(abi.encodePacked(_diamondCut[i].key));
+            checkAccess(key, msg.sender);
+            Data storage $ = diamond(key);
             for (uint j; j < _diamondCut[i].data.length; ++j) {
-                bytes4[] memory functs_ = _diamondCut[i]
-                    .data[j]
-                    .functionSelectors;
+                bytes4[] memory functs_ = _diamondCut[i].data[j].functionSelectors;
                 address facet_ = _diamondCut[i].data[j].facetAddress;
-                if (functs_.length == 0)
-                    revert IDiamond.NoSelectorsProvidedForFacetForCut(facet_);
+                if (functs_.length == 0) revert IDiamond.NoSelectorsProvidedForFacetForCut(facet_);
                 IDiamond.Action action = _diamondCut[i].data[j].action;
-                Data storage $ = diamond(
-                    keccak256(abi.encodePacked(_diamondCut[i].key))
-                );
-                if (action == IDiamond.Action.Add)
-                    $.addFunctions(facet_, functs_);
-                else if (action == IDiamond.Action.Replace)
-                    $.replaceFunctions(facet_, functs_);
-                else if (action == IDiamond.Action.Remove)
-                    $.removeFunctions(facet_, functs_);
+                if (action == IDiamond.Action.Add) $.addFunctions(facet_, functs_);
+                else if (action == IDiamond.Action.Replace) $.replaceFunctions(facet_, functs_);
+                else if (action == IDiamond.Action.Remove) $.removeFunctions(facet_, functs_);
                 else revert IDiamond.IncorrectFacetCutAction(uint8(action));
             }
             emit DiamondCut(_diamondCut[i].data, _init, _calldata);
@@ -193,22 +150,14 @@ library DiamondContractManager {
         initializeDiamondCut(_init, _calldata);
     }
 
-    function internalCut(
-        bytes4[] memory _functs,
-        bytes memory _calldata
-    ) internal {
+    function internalCut(bytes4[] memory _functs, bytes memory _calldata) internal {
         diamond(base)._addFunctions(address(this), _functs, true);
         IDiamond.Data[] memory _cut = new IDiamond.Data[](1);
         _cut[0] = IDiamond.Data(address(this), IDiamond.Action.Add, _functs);
         emit DiamondCut(_cut, address(this), _calldata);
     }
 
-    function _addFunctions(
-        Data storage $,
-        address _facet,
-        bytes4[] memory _functs,
-        bool _internal
-    ) internal {
+    function _addFunctions(Data storage $, address _facet, bytes4[] memory _functs, bool _internal) internal {
         uint16 position = uint16($.facet[_facet].functs.length);
         if (position == 0) {
             $.index[_facet] = uint16($.facets.length);
@@ -216,10 +165,7 @@ library DiamondContractManager {
         }
         for (uint i; i < _functs.length; ++i) {
             if ($.funct[_functs[i]].facet != address(0)) {
-                if (!_internal)
-                    revert IDiamond.CannotAddFunctionToDiamondThatAlreadyExists(
-                        _functs[i]
-                    );
+                if (!_internal) revert IDiamond.CannotAddFunctionToDiamondThatAlreadyExists(_functs[i]);
                 if ($.funct[_functs[i]].facet != address(this)) {
                     $.funct[_functs[i]].facet = _facet;
                 }
@@ -230,55 +176,34 @@ library DiamondContractManager {
         }
     }
 
-    function addFunctions(
-        Data storage $,
-        address _facet,
-        bytes4[] memory _functs
-    ) internal {
-        if (_facet == address(0))
-            revert IDiamond.CannotAddSelectorsToZeroAddress(_functs);
+    function addFunctions(Data storage $, address _facet, bytes4[] memory _functs) internal {
+        if (_facet == address(0)) revert IDiamond.CannotAddSelectorsToZeroAddress(_functs);
         enforcedFacetHasCode(_facet, "DiamondCut: Add facet has no code");
         $._addFunctions(_facet, _functs, false);
     }
 
-    function replaceFunctions(
-        Data storage $,
-        address _facet,
-        bytes4[] memory _functs
-    ) internal {
-        if (_facet == address(0))
-            revert IDiamond.CannotReplaceFunctionsFromFacetWithZeroAddress(
-                _functs
-            );
+    function replaceFunctions(Data storage $, address _facet, bytes4[] memory _functs) internal {
+        if (_facet == address(0)) revert IDiamond.CannotReplaceFunctionsFromFacetWithZeroAddress(_functs);
         enforcedFacetHasCode(_facet, "DiamondCut: Replace facet has no code");
         uint16 position = uint16($.facet[_facet].functs.length);
         if (_functs.length > 0 && position > 0) {
             for (uint i; i < _functs.length; ++i) {
                 Funct memory old = $.funct[_functs[i]];
-                if (old.facet == _facet)
-                    revert IDiamond
-                        .CannotReplaceFunctionWithTheSameFunctionFromTheSameFacet(
-                            _functs[i]
-                        );
+                if (old.facet == _facet) revert IDiamond.CannotReplaceFunctionWithTheSameFunctionFromTheSameFacet(_functs[i]);
                 if (old.facet == address(0)) {
                     $.funct[_functs[i]].facet = _facet;
                     $.facet[_facet].functs.push(_functs[i]);
                 }
                 // can't replace immutable functions -- functions defined directly in the diamond in this case
-                if (old.facet == address(this))
-                    revert IDiamond.CannotReplaceImmutableFunction(_functs[i]);
+                if (old.facet == address(this)) revert IDiamond.CannotReplaceImmutableFunction(_functs[i]);
 
                 // delete old functions
                 if ($.facet[old.facet].functs.length > 1) {
                     uint last = $.facet[old.facet].functs.length - 1;
-                    $.facet[old.facet].functs[old.position] = $
-                        .facet[old.facet]
-                        .functs[last];
+                    $.facet[old.facet].functs[old.position] = $.facet[old.facet].functs[last];
                     $.facet[old.facet].functs.pop();
                 } else {
-                    $.facets[$.index[old.facet]] = $.facets[
-                        $.facets.length - 1
-                    ];
+                    $.facets[$.index[old.facet]] = $.facets[$.facets.length - 1];
                     $.facets.pop();
                     delete $.facet[old.facet];
                     delete $.index[old.facet];
@@ -295,23 +220,15 @@ library DiamondContractManager {
         else revert IDiamond.ConnotReplaceWithWrongArguments(_facet);
     }
 
-    function removeFunctions(
-        Data storage $,
-        address _facet,
-        bytes4[] memory _functs
-    ) internal {
+    function removeFunctions(Data storage $, address _facet, bytes4[] memory _functs) internal {
         uint position = $.facet[_facet].functs.length;
-        if (position == 0)
-            revert IDiamond
-                .CannotRemoveFunctionFromFacetAddressThatDoesNotExist(_facet);
+        if (position == 0) revert IDiamond.CannotRemoveFunctionFromFacetAddressThatDoesNotExist(_facet);
         for (uint i; i < _functs.length; ++i) {
             bytes4 funct_ = _functs[i];
             Funct memory old = $.funct[funct_];
-            if (old.facet == address(0))
-                revert IDiamond.CannotRemoveFunctionThatDoesNotExist(funct_);
+            if (old.facet == address(0)) revert IDiamond.CannotRemoveFunctionThatDoesNotExist(funct_);
             // can't remove immutable functions -- functions defined directly in the diamond
-            if (old.facet == address(this))
-                revert IDiamond.CannotRemoveImmutableFunction(funct_);
+            if (old.facet == address(this)) revert IDiamond.CannotRemoveImmutableFunction(funct_);
             // replace funct with last funct
             if ($.facet[_facet].functs.length > 1) {
                 --position;
@@ -332,10 +249,7 @@ library DiamondContractManager {
         }
     }
 
-    function initializeDiamondCut(
-        address _init,
-        bytes memory _calldata
-    ) internal {
+    function initializeDiamondCut(address _init, bytes memory _calldata) internal {
         if (_init == address(0)) return;
         enforcedFacetHasCode(_init, "DiamondCut: _init address has no code");
         (bool success, bytes memory error) = _init.delegatecall(_calldata);
@@ -348,22 +262,15 @@ library DiamondContractManager {
                     revert(add(32, error), returndata_size)
                 }
             } else {
-                revert IDiamond.InitializationFunctionReverted(
-                    _init,
-                    _calldata
-                );
+                revert IDiamond.InitializationFunctionReverted(_init, _calldata);
             }
     }
 
-    function enforcedFacetHasCode(
-        address _facet,
-        string memory _errorMessage
-    ) internal view {
+    function enforcedFacetHasCode(address _facet, string memory _errorMessage) internal view {
         uint size;
         assembly {
             size := extcodesize(_facet)
         }
-        if (size == 0)
-            revert IDiamond.NoBytecodeAtAddress(_facet, _errorMessage);
+        if (size == 0) revert IDiamond.NoBytecodeAtAddress(_facet, _errorMessage);
     }
 }
--- utils/DiamondLoupe.sol
+++ utils/DiamondLoupe.sol
@@ -6,9 +6,9 @@ pragma solidity ^0.8.24;
 * Lightweight version of EIP-2535 Diamonds
 \******************************************************************************/
 
-import {DiamondBase} from "./DiamondBase.sol";
-import {DiamondContractManager} from "../DiamondContractManager.sol";
-import {IDiamondLoupe} from "../interfaces/IDiamondLoupe.sol";
+import { DiamondBase } from "./DiamondBase.sol";
+import { DiamondContractManager } from "../DiamondContractManager.sol";
+import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
 
 abstract contract DiamondLoupe is DiamondBase {
     using DiamondContractManager for bytes32;
@@ -33,105 +33,69 @@ abstract contract DiamondLoupe is DiamondBase {
         }
     }
 
-    function facets()
-        public
-        view
-        virtual
-        returns (DiamondContractManager.Facet[] memory)
-    {
+    function facets() public view virtual returns (DiamondContractManager.Facet[] memory) {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.getFacets()
-                : IDiamondLoupe(diamond).facets(_this);
+        return diamond == address(this) 
+            ? _this.getFacets() 
+            : IDiamondLoupe(diamond).facets(_this);
     }
 
-    function facets(
-        bytes32 _contract
-    ) public view virtual returns (DiamondContractManager.Facet[] memory) {
+    function facets(bytes32 _contract) public view virtual returns (DiamondContractManager.Facet[] memory) {
         return _contract.getFacets();
     }
 
-    function facetFunctionSelectors(
-        address _facet
-    ) public view virtual returns (bytes4[] memory) {
+    function facetFunctionSelectors(address _facet) public view virtual returns (bytes4[] memory) {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.functs(_facet)
-                : IDiamondLoupe(diamond).facetFunctionSelectors(_this, _facet);
+        return diamond == address(this) 
+            ? _this.functs(_facet) 
+            : IDiamondLoupe(diamond).facetFunctionSelectors(_this, _facet);
     }
 
-    function facetFunctionSelectors(
-        bytes32 _contract,
-        address _facet
-    ) public view virtual returns (bytes4[] memory) {
+    function facetFunctionSelectors(bytes32 _contract, address _facet) public view virtual returns (bytes4[] memory) {
         return _contract.functs(_facet);
     }
 
     function facetAddresses() public view virtual returns (address[] memory) {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.facets()
-                : IDiamondLoupe(diamond).facetAddresses(_this);
+        return diamond == address(this) 
+            ? _this.facets() 
+            : IDiamondLoupe(diamond).facetAddresses(_this);
     }
 
-    function facetAddresses(
-        bytes32 _contract
-    ) public view virtual returns (address[] memory) {
+    function facetAddresses(bytes32 _contract) public view virtual returns (address[] memory) {
         return _contract.facets();
     }
 
     function facetAddress(bytes4 _funct) public view virtual returns (address) {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.facet(_funct)
-                : IDiamondLoupe(diamond).facetAddress(_this, _funct);
+        return diamond == address(this) 
+            ? _this.facet(_funct) 
+            : IDiamondLoupe(diamond).facetAddress(_this, _funct);
     }
 
-    function facetAddress(
-        bytes32 _contract,
-        bytes4 _funct
-    ) public view virtual returns (address) {
+    function facetAddress(bytes32 _contract, bytes4 _funct) public view virtual returns (address) {
         return _contract.facet(_funct);
     }
 
-    function supportsInterface(
-        bytes4 _interface
-    ) public view virtual returns (bool) {
+    function supportsInterface(bytes4 _interface) public view virtual returns (bool) {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.checkInterface(_interface)
-                : IDiamondLoupe(diamond).supportsInterface(_this, _interface);
+        return diamond == address(this)
+            ? _this.checkInterface(_interface)
+            : IDiamondLoupe(diamond).supportsInterface(_this, _interface);
     }
 
-    function supportsInterface(
-        bytes32 _contract,
-        bytes4 _interface
-    ) public view virtual returns (bool) {
+    function supportsInterface(bytes32 _contract, bytes4 _interface) public view virtual returns (bool) {
         return _contract.checkInterface(_interface);
     }
 
     function setInterface(bytes4 _interface, bool _state) public virtual {
         address payable diamond = _this.diamond().addr;
-        return
-            diamond == address(this)
-                ? _this.setInterface(_this, _interface, _state)
-                : IDiamondLoupe(diamond).setInterface(
-                    _this,
-                    _interface,
-                    _state
-                );
+        return diamond == address(this)
+            ? _this.setInterface(_interface, _state)
+            : IDiamondLoupe(diamond).setInterface(_this, _interface, _state);
     }
 
-    function setInterface(
-        bytes32 _contract,
-        bytes4 _interface,
-        bool _state
-    ) public virtual {
-        return _this.setInterface(_contract, _interface, _state);
+    function setInterface(bytes32 _contract, bytes4 _interface, bool _state) public virtual {
+        return _contract.setInterface(_interface, _state);
     }
 }
